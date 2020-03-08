193ed5ed57a126af27555c6c774eb879780a6888
Andy Foster <andyfoster@gmail.com>
2020-03-08 18:07:20 -0400

    New Commit
    new commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app" dark>
+    <HeaderPartial v-if="hideHeader!=true"></HeaderPartial>
+    <slot/>
+    <NavbarPartial :disableScroll="disableScroll" ></NavbarPartial>
+    <FooterPartial></FooterPartial>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+import HeaderPartial from '~/layouts/partials/HeaderWithNavbar.vue'
+import NavbarPartial from '~/layouts/partials/Navbar.vue'
+import FooterPartial from '~/layouts/partials/Footer.vue'
+
+export default {
+  props: {
+    hideHeader: {
+      type: Boolean,
+      default: false
+    },
+    disableScroll: {
+      type: Boolean,
+      default: false
+    }
+  },
+  components: {
+    HeaderPartial,
+    NavbarPartial,
+    FooterPartial
+  },
+  
+  metaInfo: {
+    bodyAttrs: {
+      class: "m-0"
+    }
+  }
+}
+</script>
